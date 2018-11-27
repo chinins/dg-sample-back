be31b2c60b0ffd80dd9d92f1d62a4e186bfd0c44
Mehdi Lahlou <mehdi.lahlou@free.fr>
2018-11-27 18:01:25 +0100

    feat: add basic `koa` server
    Add `koa` dependency

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,22 @@
 import util from 'util';
 
+import Koa from 'koa';
+
 console.log('hello world');
 console.log('environment variables: %s', util.inspect(process.env));
+
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(
+  {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT, 10)
+  },
+  () => {
+    console.log('Koa server listening on port %d', parseInt(process.env.PORT, 10));
+  }
+);
