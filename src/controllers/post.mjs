9878a6053e6e491b9854748347b1dffdc01346c3
jayaFeola <34057551+chinins@users.noreply.github.com>
2018-11-28 12:25:56 +0100

    add route and controller to get post by name

--- src/controllers/post.mjs
+++ src/controllers/post.mjs
@@ -1,7 +1,6 @@
 import { getResource } from '../utils';
 
 export const specificPost = async ctx => {
-  console.log('specific post controller', ctx.params.name);
   try {
     const posts = await getResource('posts');
     const post = posts.filter(post => post.title === ctx.params.name);
